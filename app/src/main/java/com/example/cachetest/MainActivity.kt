86d6f2db395f60ed0d889b50486fae38f9e0e94f
Chris Hayes <chayes@memfault.com>
2023-04-11 11:05:17 -0600

    Add a few buttons and logging

--- app/src/main/java/com/example/cachetest/MainActivity.kt
+++ app/src/main/java/com/example/cachetest/MainActivity.kt
@@ -24,13 +24,17 @@ class MainActivity() : AppCompatActivity() {
 
   private val scope = MainScope()
 
+  private lateinit var cacheAdd1mbButton: Button
   private lateinit var cacheAdd10mbButton: Button
   private lateinit var cacheAdd100mbButton: Button
+  private lateinit var cacheAdd1000mbButton: Button
   private lateinit var cacheTextSize: TextView
   private lateinit var cacheClearButton: Button
 
+  private lateinit var appDataAdd1mbButton: Button
   private lateinit var appDataAdd10mbButton: Button
   private lateinit var appDataAdd100mbButton: Button
+  private lateinit var appDataAdd1000mbButton: Button
   private lateinit var appDataTextSize: TextView
   private lateinit var appDataClearButton: Button
 
@@ -39,41 +43,57 @@ class MainActivity() : AppCompatActivity() {
 
   private var storageMonitorJob: Job? = null
 
+  lateinit var totalSpaceText: String
+  lateinit var allocatableSpaceText: String
+  lateinit var appDataSizeText: String
+  lateinit var cacheSizeText: String
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
+    cacheAdd1mbButton = findViewById(R.id.add_cache_1mb)
+    cacheAdd1mbButton.setOnClickListener {
+      scope.launch { allocateMegabytes(1, CACHE) }
+    }
     cacheAdd10mbButton = findViewById(R.id.add_cache_10mb)
     cacheAdd10mbButton.setOnClickListener {
       scope.launch { allocateMegabytes(10, CACHE) }
     }
-
     cacheAdd100mbButton = findViewById(R.id.add_cache_100mb)
     cacheAdd100mbButton.setOnClickListener {
       scope.launch { allocateMegabytes(100, CACHE) }
     }
+    cacheAdd1000mbButton = findViewById(R.id.add_cache_1000mb)
+    cacheAdd1000mbButton.setOnClickListener {
+      scope.launch { allocateMegabytes(1000, CACHE) }
+    }
     cacheTextSize = findViewById(R.id.cache_size)
-
     cacheClearButton = findViewById(R.id.clear_cache_data)
     cacheClearButton.setOnClickListener {
       clearFiles(CACHE)
     }
 
+    appDataAdd1mbButton = findViewById(R.id.add_app_data_1mb)
+    appDataAdd1mbButton.setOnClickListener {
+      scope.launch { allocateMegabytes(1, APP_DATA) }
+    }
     appDataAdd10mbButton = findViewById(R.id.add_app_data_10mb)
     appDataAdd10mbButton.setOnClickListener {
       scope.launch { allocateMegabytes(10, APP_DATA) }
     }
-
     appDataAdd100mbButton = findViewById(R.id.add_app_data_100mb)
     appDataAdd100mbButton.setOnClickListener {
       scope.launch { allocateMegabytes(100, APP_DATA) }
     }
-
+    appDataAdd1000mbButton = findViewById(R.id.add_app_data_1000mb)
+    appDataAdd1000mbButton.setOnClickListener {
+      scope.launch { allocateMegabytes(1000, APP_DATA) }
+    }
     appDataClearButton = findViewById(R.id.clear_app_data)
     appDataClearButton.setOnClickListener {
       clearFiles(APP_DATA)
     }
-
     allocatableSpaceTextView = findViewById(R.id.allocatable_space)
     totalSpaceTextView = findViewById(R.id.total_space)
 
@@ -105,7 +125,7 @@ class MainActivity() : AppCompatActivity() {
 
     val file = File(dir, UUID.randomUUID().toString() + ".bin")
     try {
-      for (i in 0..sizeMegabytes) {
+      for (i in 1..sizeMegabytes) {
         val bytes = Random.nextUBytes(MEGABYTE).asByteArray()
         file.appendBytes(bytes)
       }
@@ -117,8 +137,10 @@ class MainActivity() : AppCompatActivity() {
   }
 
   private fun setSizesTextViews() = scope.launch {
-    totalSpaceTextView.text = (filesDir.totalSpace / MEGABYTE).toString() + " mb"
-    allocatableSpaceTextView.text = (filesDir.usableSpace / MEGABYTE).toString() + " mb"
+    totalSpaceText = (filesDir.totalSpace / MEGABYTE).toString() + "mb"
+    totalSpaceTextView.text = totalSpaceText
+    allocatableSpaceText = (filesDir.usableSpace / MEGABYTE).toString() + "mb"
+    allocatableSpaceTextView.text = allocatableSpaceText
 
     var cacheSize: Long = 0
     for (cachedFile  in cacheDir.listFiles()) {
@@ -126,7 +148,8 @@ class MainActivity() : AppCompatActivity() {
       // Log.d(TAG, "$cachedFile - $size")
       cacheSize += size
     }
-    cacheTextSize.text = (cacheSize / MEGABYTE).toString() + " mb"
+    cacheSizeText = (cacheSize / MEGABYTE).toString() + "mb"
+    cacheTextSize.text = cacheSizeText
 
     var appDataSize: Long = 0
     for (appFile  in filesDir.listFiles()) {
@@ -134,15 +157,27 @@ class MainActivity() : AppCompatActivity() {
       // Log.d(TAG, "$appFile - $size")
       appDataSize += size
     }
-    appDataTextSize.text = (appDataSize / MEGABYTE).toString() + " mb"
+    appDataSizeText = (appDataSize / MEGABYTE).toString() + "mb"
+    appDataTextSize.text = appDataSizeText
+  }
+
+  private fun logSizes() = scope.launch {
+    Log.d(TAG, "%s: %s".format("allocatableSpace", allocatableSpaceText))
+    Log.d(TAG, "%s: %s".format("totalSpace", totalSpaceText))
+    Log.d(TAG, "%s: %s".format("cacheSize", cacheSizeText))
+    Log.d(TAG, "%s: %s".format("appData", appDataSizeText))
   }
 
   private fun startStorageMonitorJob() {
     stopStorageMonitorJob()
     storageMonitorJob = scope.launch {
+      var i = 0
       while(true) {
         setSizesTextViews()
+        if (i == 0) logSizes()
         delay(250)
+        i += 1
+        i %= 4
       }
     }
   }
